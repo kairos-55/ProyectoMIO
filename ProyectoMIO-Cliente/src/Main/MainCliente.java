1730613db5f6da42c5c05fd5f54925beb9298eff
kairos-55 <jisus-55@hotmail.com>
2016-02-27 17:19:15 -0500

    Primera versión

--- ProyectoMIO-Cliente/src/Main/MainCliente.java
+++ ProyectoMIO-Cliente/src/Main/MainCliente.java
@@ -1,10 +1,6 @@
 package Main;
 
 import GUI.PaquetePrincipal.VentanaPrincipal;
-import AdministradorConexion.*;
-import Model.Messages.Reply;
-import Model.Messages.Request;
-import java.io.IOException;
 
 /**
  *
@@ -22,14 +18,7 @@ public class MainCliente {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AdministradorCliente admin = new AdministradorCliente();
         
-              
-                String sqlStatement = "SELECT placa FROM bus;";
-                Request request = new Request();
-        request.setType("QUERY");
-        request.setSqlRequest(sqlStatement);
-                request.setColumns(1);
         VentanaPrincipal ventanaPrincipal = new VentanaPrincipal(); 
         
     }
